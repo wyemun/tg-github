8c532096526cb692f0dc85325ef2687be760f893
Wyemun <wyemun@gmail.com>
2017-02-16 18:36:12 +0800

    Added HTTP responses processors

--- src/utils/index.js
+++ src/utils/index.js
@@ -11,3 +11,17 @@ export function createReducer (initialState, reducerMap) {
     return reducer ? reducer(state, action.payload) : state
   }
 }
+
+export function checkHttpStatus (response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response
+  } else {
+    var error = new Error(response.statusText)
+    error.response = response
+    throw error
+  }
+}
+
+export function parseJSON (response) {
+  return response.json()
+}
